ad2ba6f4d3ffcdc4ef6a0a8dd4cdcf474c67de1b
st4swift <52103510+st4swift@users.noreply.github.com>
2019-11-30 21:48:24 +0800

    Update VPS-debian10-first-install-v2ray-trojan.sh

--- VPS-debian10-first-install-v2ray-trojan.sh
+++ VPS-debian10-first-install-v2ray-trojan.sh
@@ -8,31 +8,6 @@ if [[ $(id -u) != 0 ]]; then
     exit 1
 fi
 
-# 更换.bashrc
-cat > .bashrc << EOF
-# ~/.bashrc: executed by bash(1) for non-login shells.
-
-# Note: PS1 and umask are already set in /etc/profile. You should not
-# need this unless you want different defaults for root.
-PS1='${debian_chroot:+($debian_chroot)}\h:\w\$ '
-umask 022
-
-# You may uncomment the following lines if you want `ls' to be colorized:
-export LS_OPTIONS='--color=auto'
-eval "`dircolors`"
-alias ls='ls $LS_OPTIONS'
-alias ll='ls $LS_OPTIONS -l'
-alias l='ls $LS_OPTIONS -lA'
-#
-# Some more alias to avoid making mistakes:
-alias rm='rm -i'
-alias cp='cp -i'
-alias mv='mv -i'
-
-EOF
-
-source .bashrc
-
 
 # 更新package,并安装一些工具
 apt update && apt upgrade -y
@@ -155,7 +130,7 @@ sed -i "s/_passwd1_/$pswd1/;s/_passwd2_/$pswd2/;s/_passwd3_/$pswd3/;s/_passwd4_/
 
 cd 
 
-servicr v2ray restart
+service v2ray start
 
 echo All Done!
 
