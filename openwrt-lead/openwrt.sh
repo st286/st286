f4621e18bb0e9f66b081d58e5b8e8f62968bf87a
st4swift <52103510+st4swift@users.noreply.github.com>
2020-02-16 14:51:37 +0800

    Update openwrt.sh

--- openwrt-lead/openwrt.sh
+++ openwrt-lead/openwrt.sh
@@ -3,6 +3,13 @@
 ## 编译Lean的Openwrt固件全攻略   https://imgki.com/archives/openwrt-lean.html
 ## Lean's OpenWrt source    https://github.com/coolsnowwolf/lede
 
+## 用VPS 40G 2CPU 4G，Ubuntu 18.04
+## Vultr
+
+## 不要用 root 用户 git 和编译
+## 国内用户编译前最好准备好梯子
+## 默认登陆IP 192.168.1.1, 密码 password
+
 
 USER_NAME=st
 
@@ -22,7 +29,7 @@ echo "visudo /etc/sudoers: add $USER_NAME    ALL=(ALL:ALL) ALL"
 visudo
 
 
-
+## 转换非root用户
 su $USER_NAME
 
 
@@ -33,17 +40,22 @@ sudo apt-get -y install build-essential asciidoc binutils bzip2 gawk gettext git
 
 sudo apt-get -y install build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint
 
-sudo apt-get update
-
-sudo apt-get -y install build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint device-tree-compiler
-
-sudo apt-get -y install build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint
+## do again
 
 sudo apt-get update
 
 sudo apt-get -y install build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint device-tree-compiler
 
 sudo apt-get -y install build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint
+
+## do again
+
+sudo apt-get update
+
+sudo apt-get -y install build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint device-tree-compiler
+
+sudo apt-get -y install build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint
+
 
 
 cd /home/$USER_NAME
@@ -57,13 +69,12 @@ sudo ./scripts/feeds update -a
 sudo ./scripts/feeds install -a
 
 
-
 sudo ./scripts/feeds update -a
 
 sudo ./scripts/feeds install -a
 
 
-
 sudo make menuconfig
 
 sudo make V=s
+
