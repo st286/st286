0993751b1ca0788cfda4575080ac31e61ae36bf1
st4swift <52103510+st4swift@users.noreply.github.com>
2020-02-21 09:45:31 +0800

    Update openwrt.sh

--- openwrt-lead/openwrt.sh
+++ openwrt-lead/openwrt.sh
@@ -50,33 +50,26 @@ sudo apt-get -y install build-essential asciidoc binutils bzip2 gawk gettext git
 
 sudo apt-get -y install build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint
 
-## do again
-
-sudo apt-get update
-
-sudo apt-get -y install build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint device-tree-compiler
-
-sudo apt-get -y install build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint
 
 
 
 cd "/home/$USER_NAME"
 
-sudo git clone https://github.com/coolsnowwolf/lede
+git clone https://github.com/coolsnowwolf/lede
 
 cd lede
 
-sudo ./scripts/feeds update -a
+./scripts/feeds update -a
 
-sudo ./scripts/feeds install -a
-
-
-sudo ./scripts/feeds update -a
-
-sudo ./scripts/feeds install -a
+./scripts/feeds install -a
 
 
-sudo make menuconfig
+./scripts/feeds update -a
 
-sudo make V=s
+./scripts/feeds install -a
+
+
+make menuconfig
+
+make V=s
 
